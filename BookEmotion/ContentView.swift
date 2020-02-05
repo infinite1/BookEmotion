5bf63116ee60eeb524781ab7056c6ebb61a76f7d
Xinyu <victorvone@gmail.com>
2020-02-05 11:04:27 +1100

    Initial Commit

---
+++ BookEmotion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookEmotion
+//
+//  Created by Xinyu Sun on 5/2/20.
+//  Copyright © 2020 Xinyu Sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
